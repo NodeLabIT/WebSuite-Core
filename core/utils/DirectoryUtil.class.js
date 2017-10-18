66dde680ca9b2c2465e2a679624fad707c11d250
Marc <marc.troll@nodelab-it.de>
2017-10-18 21:34:06 +0200

    added js-docs

--- core/utils/DirectoryUtil.class.js
+++ core/utils/DirectoryUtil.class.js
@@ -4,6 +4,13 @@ const fs = require('fs');
 
 class DirectoryUtil {
 
+    /**
+     * Check, whether directory exists or not
+     *
+     * @param path path of the directory
+     *
+     * @returns Promise resolves on success, rejects on failure with error
+     * */
     static directoryExists(path) {
         return new Promise((resolve, reject) => {
             fs.stat(path, (err) => {
@@ -17,6 +24,13 @@ class DirectoryUtil {
         });
     }
 
+    /**
+     * Get all files and directories
+     *
+     * @param path path of the directory you want to get the content of
+     *
+     * @returns Promise resolves on success with files-list, rejects on failure with error
+     * */
     static files(path) {
         return new Promise((resolve, reject) => {
             fs.readdir(path, (err, files) => {
@@ -30,9 +44,20 @@ class DirectoryUtil {
         });
     }
 
-    static createDirectory(path) {
+    /**
+     * Create a directory
+     *
+     * @param path path of the directory, where you want to create the directory in
+     * @param directoryName name of the directory, you want to create
+     *
+     * @returns Promise resolves on success, rejects on failure with error
+     * */
+    static createDirectory(path, directoryName) {
         return new Promise((resolve, reject) => {
-            fs.mkdir(path, (err) => {
+            if(!path.endsWith("/"))
+                path = path + "/";
+
+            fs.mkdir(path + directoryName, (err) => {
                 if(err) {
                     reject(err);
                     return;
@@ -43,6 +68,13 @@ class DirectoryUtil {
         });
     }
 
+    /**
+     * Remove a directory
+     *
+     * @param path path of the directory
+     *
+     * @returns Promise resolves on success, rejects on failure with error
+     * */
     static removeDirectory(path) {
         return new Promise((resolve, reject) => {
             fs.rmdir(path, (err) => {
