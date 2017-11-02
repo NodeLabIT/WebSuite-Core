45208e256bcf76fc53184569a1d8ac902d3a47e7
Marc <marc.troll@nodelab-it.de>
2017-11-02 22:56:46 +0100

    fixed submenu and added form-design

--- cp/src/components/core-configuration-general.vue
+++ cp/src/components/core-configuration-general.vue
@@ -1,6 +1,23 @@
 <template>
     <div>
-
+        <form>
+            <div>
+                <input type="text" id="input" class="tiny">
+                <label for="input">Titel der Seite</label>
+            </div>
+            <div>
+                <input type="text" id="input2" class="tiny">
+                <label for="input2">Slogan der Seite</label>
+            </div>
+            <div>
+                <input type="text" id="input3" class="large">
+                <label for="input3">Beschreibung der Seite</label>
+            </div>
+            <div>
+                <input type="text" id="input4" class="large">
+                <label for="input4">Test</label>
+            </div>
+        </form>
     </div>
 </template>
 
