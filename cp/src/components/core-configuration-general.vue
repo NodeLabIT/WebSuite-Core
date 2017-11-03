8a79a96e7a7ba0c98cfddb74c490aa0bc3bb5019
Marc <marc.troll@nodelab-it.de>
2017-11-03 15:22:25 +0100

    added simple valididation-check

--- cp/src/components/core-configuration-general.vue
+++ cp/src/components/core-configuration-general.vue
@@ -2,19 +2,19 @@
     <div>
         <form>
             <div>
-                <input type="text" id="input" class="tiny">
+                <input type="text" id="input" v-model="username" class="tiny" v-bind:class="{ valid: $root.isValid(username) }" />
                 <label for="input">Titel der Seite</label>
             </div>
             <div>
-                <input type="text" id="input2" class="tiny">
+                <input type="text" id="input2" v-model="username2" class="tiny" v-bind:class="{ valid: $root.isValid(username2) }" />
                 <label for="input2">Slogan der Seite</label>
             </div>
             <div>
-                <input type="text" id="input3" class="large">
+                <input type="text" id="input3" class="large" v-model="username3" v-bind:class="{ valid: $root.isValid(username3) }" />
                 <label for="input3">Beschreibung der Seite</label>
             </div>
             <div>
-                <input type="text" id="input4" class="large">
+                <input type="text" id="input4" class="large" v-model="username4" v-bind:class="{ valid: $root.isValid(username4) }" />
                 <label for="input4">Test</label>
             </div>
         </form>
@@ -26,7 +26,10 @@
     export default {
         data() {
             return {
-
+                username: "",
+                username2: "",
+                username3: "",
+                username4: ""
             }
         },
         methods: {
