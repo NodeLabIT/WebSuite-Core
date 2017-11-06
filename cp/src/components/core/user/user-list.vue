94d483571d7c0d10dd3d39706218520527db6bbb
Marc <marc.troll@nodelab-it.de>
2017-11-06 23:02:09 +0100

    changed/improved mobile-design

--- cp/src/components/core/user/user-list.vue
+++ cp/src/components/core/user/user-list.vue
@@ -1,22 +1,27 @@
 <template>
     <div>
-        <a class="floating-button"><i class="material-icons">add</i></a>
-        <table>
-            <thead class="uppercase">
+        <a class="floating-button"><i class="material-icons">add</i><span class="text">Mitglied hinzufügen</span></a>
+        <div class="table-container">
+            <table>
+                <thead class="uppercase">
                 <tr>
-                    <td style="width: 48px;">
-                        <i class="material-icons" v-if="selectedUsers.length === 0" @click="selectAll()">check_box_outline_blank</i>
-                        <i class="material-icons" v-if="selectedUsers.length > 0" @click="deselectAll()">indeterminate_check_box</i>
+                    <td class="row-1">
+                        <a v-if="selectedUsers.length === 0" @click="selectAll()">
+                            <i class="material-icons">check_box_outline_blank</i>
+                        </a>
+                        <a v-if="selectedUsers.length > 0" @click="deselectAll()">
+                            <i class="material-icons">indeterminate_check_box</i>
+                        </a>
                     </td>
-                    <td style="width: calc(40% - 48);"></td>
-                    <td style="width: 12.5%;">Rang</td>
-                    <td style="width: 7.5%;">Punkte</td>
-                    <td style="width: calc(20% - 24px);">registriert</td>
-                    <td style="width: calc(20% - 24px);">letzte aktivität</td>
-                    <td style="width: 48px;"></td>
+                    <td class="row-2"></td>
+                    <td class="hidden-tiny row-3">Rang</td>
+                    <td class="hidden-tiny row-4">Punkte</td>
+                    <td class="hidden-tiny row-5">registriert</td>
+                    <td class="hidden-tiny row-6">letzte aktivität</td>
+                    <td class="row-7"></td>
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 <tr v-for="user in users">
                     <td>
                         <i class="material-icons" v-if="!isSelected(user.userID)" @click="toggleSelection(user.userID)">check_box_outline_blank</i>
@@ -29,14 +34,15 @@
                             <span>{{ user.email }}</span>
                         </div>
                     </td>
-                    <td>Administrator</td>
-                    <td>6132</td>
-                    <td>am 04.01.2017, 13:57 Uhr</td>
-                    <td>vor 17 Minuten</td>
+                    <td class="hidden-tiny">Administrator</td>
+                    <td class="hidden-tiny">6132</td>
+                    <td class="hidden-tiny">am 04.01.2017, 13:57 Uhr</td>
+                    <td class="hidden-tiny">vor 17 Minuten</td>
                     <td><i class="material-icons">more_horiz</i></td>
                 </tr>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
+        </div>
         <div class="pagination">
             <router-link :to="`/user/user-list/${pageID - 1}`" v-if="pageID > 1"><i class="material-icons">navigate_before</i></router-link>
             <a v-if="pageID === 1" class="inactive"><i class="material-icons">navigate_before</i></a>
@@ -50,6 +56,34 @@
         </aside>
     </div>
 </template>
+
+<style>
+    .row-1, .row-7 {
+        width: 48px;
+    }
+    @media all and (min-width: 1081px) {
+        .row-2 {
+            width: calc(40% - 48);
+        }
+        .row-3 {
+            width: 12.5%;
+        }
+        .row-4 {
+            width: 7.5%;
+        }
+        .row-5 {
+            width: calc(20% - 24px);
+        }
+        .row-6 {
+            width: calc(20% - 24px);
+        }
+    }
+    @media all and (max-width: 1081px) {
+        .row-2 {
+            width: calc(100% - 96px);
+        }
+    }
+</style>
 
 <script>
     import { sio } from '../../../main';
