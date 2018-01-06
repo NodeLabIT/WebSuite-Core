2a8e6522c8e83b29277bb6588b9f11901419aafb
Marc <marc.troll@nodelab-it.de>
2018-01-06 02:06:19 +0100

    added user-saving on registration

--- core/websocket/eventListener/register.class.js
+++ core/websocket/eventListener/register.class.js
@@ -5,7 +5,7 @@ const request = require('request');
 class Register {
 
     static listen() {
-        WebSuite.getWebSocketHandler().registerEvent('login', (socket, data, address) => {
+        WebSuite.getWebSocketHandler().registerEvent('register', (socket, data, address) => {
             WebSuite.getDatabase().query("SELECT COUNT(*) AS count FROM wsFailedLogins WHERE ipAddress=? AND unixtime>=? AND type='registration'", [address, TimeUtil.currentTime() - 12 * 60 * 60 * 1000]).then(count => {
                 if (parseInt(count[0].count) < 3) {
                     const secretKey = require('../../../config.json').secretKey;
@@ -15,7 +15,7 @@ class Register {
                         body = JSON.parse(body);
 
                         if (body.success !== undefined && !body.success) {
-                            WebSuite.getWebSocketHandler().sendToClient(socket, 'login', {
+                            WebSuite.getWebSocketHandler().sendToClient(socket, 'register', {
                                 err: "captcha failed",
                                 id: -1
                             });
@@ -23,15 +23,56 @@ class Register {
                             return;
                         }
 
-                        // TODO:
+                        // TODO: Registration
+                        UserUtil.usernameValid(data.username).then(() => {
+                            UserUtil.emailValid(data.email).then(() => {
+                                UserUtil.usernameAvailable(data.username).then(() => {
+                                    UserUtil.emailAvailable(data.email).then(() => {
+                                        const salt = CryptoUtil.generateSalt(64);
+                                        const password = CryptoUtil.hashPassword(data.password, salt);
+                                        WebSuite.getDatabase().query("INSERT INTO wsUser(username, email, password) VALUES(?, ?, ?)", [data.username, data.email, password]).then(insert => {
+                                            const userID = insert.insertId;
+
+                                            FileUtil.readFile(_dir + '/data/userSalts.json').then(userSalts => {
+                                                userSalts = JSON.parse(userSalts);
+                                                userSalts.push({userID: salt});
+
+                                                FileUtil.saveFile(_dir + '/data/userSalts.json', JSON.stringify(userSalts, null, 2)).then(saved => {
+                                                    // success
+                                                }).catch(err => {
+                                                    // TODO
+                                                });
+                                            }).catch(err => {
+                                                // TODO
+                                            });
+                                        }).catch(err => {
+                                            // TODO
+                                        });
+                                    }).catch(err => {
+                                        // TODO
+                                    });
+                                }).catch(err => {
+                                    // TODO
+                                });
+                            }).catch(err => {
+                                // TODO
+                            });
+                        }).catch(err => {
+                            // TODO
+                        });
                     });
                 } else {
-                    WebSuite.getWebSocketHandler().sendToClient(socket, 'login', {err: "blocked by server", id: -1});
+                    WebSuite.getWebSocketHandler().sendToClient(socket, 'register', {
+                        err: "blocked by server",
+                        id: -1
+                    });
                 }
             }).catch(err => {
-                WebSuite.getWebSocketHandler().sendToClient(socket, 'login', {err: "servererror", id: -1});
+                WebSuite.getWebSocketHandler().sendToClient(socket, 'register', {
+                    err: "servererror",
+                    id: -1
+                });
                 WebSuite.getLogger().error(err);
-
             });
         });
     }
