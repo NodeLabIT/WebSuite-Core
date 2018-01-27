184e7d258c2cea84063d239cfe780557edc6cd84
Marc <marc.troll@nodelab-it.de>
2018-01-28 00:09:46 +0100

    changed gulpfile for packaging components

--- gulpfile.js
+++ gulpfile.js
@@ -4,23 +4,39 @@ const gulp = require('gulp'),
     del = require('del');
 
 gulp.task('package-core', function () {
-    return gulp.src('./core')
+    return gulp.src('./core/**')
         .pipe(tar('core.tar'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gzip())
+        .pipe(gulp.dest('temp'));
 });
 
 gulp.task('package-system', function () {
-    return gulp.src('./system')
+    return gulp.src('./system/**')
         .pipe(tar('system.tar'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gzip())
+        .pipe(gulp.dest('temp'));
+});
+
+gulp.task('package-cp', function () {
+    return gulp.src('./cp/**')
+        .pipe(tar('cp.tar'))
+        .pipe(gzip())
+        .pipe(gulp.dest('temp'));
+});
+
+gulp.task('package-frontend', function () {
+    return gulp.src('./frontend/**')
+        .pipe(tar('frontend.tar'))
+        .pipe(gzip())
+        .pipe(gulp.dest('temp'));
 });
 
 gulp.task('clean', function () {
-    return del('dist/');
+    return del('temp/');
 });
 
-gulp.task('package', ['package-core', 'package-system'], function () {
-    return gulp.src(['core.tar', 'system.tar'], {base: 'dist'})
+gulp.task('package', ['package-core', 'package-system', 'package-cp', 'package-frontend'], function () {
+    return gulp.src(['temp/*.tar.gz', 'index.js', 'package.json', 'config.example.json'])
         .pipe(tar('websuite.tar'))
         .pipe(gzip())
         .pipe(gulp.dest('dist'));
