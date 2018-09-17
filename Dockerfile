023f9b2538e23b3758ea2c224d853276aed3b2de
ilouHD <marc.troll@nodelab-it.de>
2018-09-17 22:36:45 +0200

    changed Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -23,17 +23,19 @@ COPY cp /opt/websuite/cp
 COPY frontend /opt/websuite/frontend
 COPY system /opt/websuite/system
 
+RUN cd frontend && npm install && npm cache clean --force && cd ..
+RUN cd cp && npm install && npm cache clean --force && cd ..
+
 COPY index.js /opt/websuite/index.js
 COPY docker.js /opt/websuite/docker.js
 
 COPY data/permissionsList/* /opt/websuite/default/permissionsList/
 COPY data/options.json /opt/websuite/default/options.json
 COPY data/plugins.json /opt/websuite/default/plugins.json
+COPY data/footerScript.js /opt/websuite/default/footerScript.js
 COPY data/config.example.json /opt/websuite/default/config.json
 
-CMD node docker.js
-
-CMD node index.js
+CMD ["sh", "-c", "node docker.js && node index.js"]
 
 EXPOSE 8080
 EXPOSE 8081
--- docker.js
+++ docker.js
@@ -11,17 +11,18 @@ console.log("Copying files to docker-volume /opt/websuite/data");
 const files = {
 	"default/permissionsList/administrativePermissions.json": "/opt/websuite/data/permissionsList/administrativePermissions.json",
 	"default/permissionsList/permissions.json": "/opt/websuite/data/permissionsList/permissions.json",
-	"default/config.example.json": "/opt/websuite/data/config.json",
+	"default/config.json": "/opt/websuite/data/config.json",
 	"default/footerScript.js": "/opt/websuite/data/footerScript.js",
 	"default/options.json": "/opt/websuite/data/options.json",
 	"default/plugins.json": "/opt/websuite/data/plugins.json"
 };
 
+fs.mkdirSync("/opt/websuite/data/permissionsList");
+
 for (const file in files) {
 	console.log("Copying " + file + " to " + files[file]);
 	if(!fs.existsSync(files[file])) {
-		console.log("COPYING FILES");
-		fs.copyFileSync(file, files[file], COPYFILE_EXCL);
+		fs.copyFileSync(file, files[file], fs.constants.COPYFILE_EXCL);
 		continue;
 	}
 	console.log("File already exists in destination. Checking for Updates...");
